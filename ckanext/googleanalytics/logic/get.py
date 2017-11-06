90c0fce3270b857aff05a2edc80d5e461416414b
Mikko Nieminen <mikko.nieminen@gofore.com>
2017-11-06 13:52:46 +0200

    Add extra fields to most visited packages action

--- ckanext/googleanalytics/logic/get.py
+++ ckanext/googleanalytics/logic/get.py
@@ -14,4 +14,15 @@ def most_visited_packages(context, data_dict):
         start_date = datetime.strptime(start_date, "%d-%m-%Y")
     if end_date:
         end_date = datetime.strptime(end_date, "%d-%m-%Y")
-    return model.PackageStats.get_top(start_date=start_date, end_date=end_date, dataset_type=dataset_type)
+
+    result = model.PackageStats.get_top(start_date=start_date, end_date=end_date, dataset_type=dataset_type)
+    packages = []
+
+    for package in result['packages']:
+        package_with_extras = toolkit.get_action('package_show')(context, { 'id': package['package_id'] })
+        package_with_extras['visits'] = package['visits']
+        package_with_extras['visit_date'] = package['visit_date']
+        packages.append(package_with_extras)
+
+    result['packages'] = packages
+    return result
